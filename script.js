cfff76ff61590f06c5c2fa7d78858c219142bb3c
Davi Ribeiro <daviribeiro442@gmail.com>
2023-10-05 14:17:55 -0300

    Corrigindo texto da pagina

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//Nome: Davi Ribeiro, Nikolas da Silva, Rodolfo Santilio
+
+//Calculadora de valor de parcela de emprestimo com taxa fixa de 40% ao ano
+
+//Usuario deve informar o valor do emprestimo e a quantidade de parcelas do mesmo
+
+//valorEmprestimo - Variavel de entrada utilizada para guardar o valor do emprestimo
+//quantidadeParcelas - Variavel de entrada utilizada para guardar a quantidade de parcelas
+//taxaAnualFixa - Constante que guarda o valor da taxa fixa anual
+//taxaMensal - Constante que guarda o valor da taxa mensal
+//valorParcela - Variavel que guarda o valor das parcelas mensais
+//valorTotalPago - Variavel que guarda o valor total pago do emprestimo
+//mensalDiv - Variavel utilizada para escrever o valor das parcelas em uma div
+//totalDiv - Variavel utilizada para escrever o valor total pago em um div
 
 //Chamando função
 function calcularEmprestimo() {
@@ -22,14 +36,14 @@ function calcularEmprestimo() {
   }
 
 //Definindo as taxas anuais e mensais
-  var taxaAnualFixa = 0.4; // 40% ao ano
-  var taxaMensal = taxaAnualFixa / 12;
+  const taxaAnualFixa = 0.4; // 40% ao ano
+  const taxaMensal = taxaAnualFixa / 12;
 
 //Cálculo da parcela
   var valorParcela =
     (valorEmprestimo *
-      (taxaMensal * Math.pow(1 + taxaMensal, quantidadeParcelas))) /
-    (Math.pow(1 + taxaMensal, quantidadeParcelas) - 1);
+      taxaMensal * Math.pow(1 + taxaMensal, quantidadeParcelas)) /
+    (Math.pow(1 + taxaMensal, quantidadeParcelas) - 1 );
 
 //Calculando o total pago do emprestimo
   var valorTotalPago = valorParcela * quantidadeParcelas;
