3134e8c5bd544716c1c0e5d2a26ff5f624869f4e
Davi Ribeiro <daviribeiro442@gmail.com>
2023-10-05 10:13:22 -0300

    Adicionando comentarios no script

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+
+//Chamando função
 function calcularEmprestimo() {
+  
+//Pegando variáveis apartir do ID
   var valorEmprestimo = parseFloat(
     document.getElementById("valorEmprestimo").value
   );
@@ -6,6 +10,7 @@ function calcularEmprestimo() {
     document.getElementById("quantidadeParcelas").value
   );
 
+//Garantindo que os campos não tenham valores inválidos  
   if (
     isNaN(valorEmprestimo) ||
     isNaN(quantidadeParcelas) ||
@@ -16,18 +21,22 @@ function calcularEmprestimo() {
     return;
   }
 
+//Definindo as taxas anuais e mensais
   var taxaAnualFixa = 0.4; // 40% ao ano
   var taxaMensal = taxaAnualFixa / 12;
 
+//Cálculo da parcela
   var valorParcela =
     (valorEmprestimo *
       (taxaMensal * Math.pow(1 + taxaMensal, quantidadeParcelas))) /
     (Math.pow(1 + taxaMensal, quantidadeParcelas) - 1);
+
+//Calculando o total pago do emprestimo
   var valorTotalPago = valorParcela * quantidadeParcelas;
 
+//Colocando o resultado dentro das divs
   var mensalDiv = document.getElementById("mensal");
-  mensalDiv.innerHTML = valorParcela.toFixed(2);
-
+  mensalDiv.innerHTML = "R$" + valorParcela.toFixed(2);
   var totalDiv = document.getElementById("total");
-  total.innerHTML = valorTotalPago.toFixed(2);
+  total.innerHTML = "R$" + valorTotalPago.toFixed(2);
 }
